18b0a5ae3e14e50d7c7feb2437a9435129683075
Salma Mohammed <sm.71175@gmail.com>
2018-01-24 15:40:18 +0200

    Refactor: use stateless function for bookshelf component

--- src/Bookshelf.js
+++ src/Bookshelf.js
@@ -1,33 +1,30 @@
 import React from 'react';
 import Book from './Book'
 
-
-class Bookshelf extends React.Component {
-  render() {
+function Bookshelf(props) {
   	return(
     	<div className="bookshelf">
         <h2 className="bookshelf-title">
-          {this.props.shelftitle}
+          {props.shelftitle}
         </h2>
         <div className="bookshelf-books">
           <ol className="books-grid">
-            {this.props.books.map(book =>
+            {props.books.map(book =>
               <Book
               key={book.id}
-              books={this.props.books}
+              books={props.books}
               bookId={book.id}
               bookThumbnail={(book.imageLinks) ? book.imageLinks.thumbnail : null}
               bookShelf={book.shelf}
               bookTitle={book.title}
               bookAuthor={book.authors}
-              onHandler={this.props.onChange}
+              onHandler={props.onChange}
               />
             )}
           </ol>
         </div>
       </div>
     )
-  }
 }
 
 export default Bookshelf
